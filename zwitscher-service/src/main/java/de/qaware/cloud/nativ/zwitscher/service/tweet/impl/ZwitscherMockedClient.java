de653c7f4178b62f04a67bbbc22500ed49117de6
Lukas Weber <lukas.b.weber@student.hs-rm.de>
2019-02-07 17:36:15 +0100

    Changed mocked zwitscher client to use constant delay

--- zwitscher-service/src/main/java/de/qaware/cloud/nativ/zwitscher/service/tweet/impl/ZwitscherMockedClient.java
+++ zwitscher-service/src/main/java/de/qaware/cloud/nativ/zwitscher/service/tweet/impl/ZwitscherMockedClient.java
@@ -2,27 +2,19 @@ package de.qaware.cloud.nativ.zwitscher.service.tweet.impl;
 
 import de.qaware.cloud.nativ.zwitscher.service.tweet.ZwitscherClient;
 import de.qaware.cloud.nativ.zwitscher.service.tweet.ZwitscherMessage;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
-import java.util.Random;
+import java.util.stream.Stream;
 
 public class ZwitscherMockedClient implements ZwitscherClient {
-    private int minDelay = 1; //ms
-    private int maxDelay = 5; //ms
-
-    private Random random = new Random();
-
-    private Duration getRandomDuration() {
-        return Duration.ofMillis(minDelay + random.nextInt(maxDelay - minDelay));
-    }
+    private Duration duration = Duration.ofMillis(200);
 
     @Override
     public Flux<ZwitscherMessage> search(String q, int pageSize) {
-        return Flux.range(1, pageSize)
-                .flatMap(i -> Mono.just(new ZwitscherMessage("Searched for: " + q + "\tnr: " + i))).delayElements(getRandomDuration());
+        return Mono.delay(duration).thenMany(Flux.fromStream(Stream.iterate(1, i->i+1)
+                        .limit(pageSize)
+                        .map(i->new ZwitscherMessage("Searched for: " + q + " nr " + i))));
     }
 }
